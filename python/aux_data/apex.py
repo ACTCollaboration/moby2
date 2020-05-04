b00011da224d83511d0121cbc4b6e8f7bbb54e7a
Matthew Hasselfield <mhasse@astro.princeton.edu>
2020-05-04 21:59:46 +0000

    aux_data: generalize APEX code for other ascii weather archives

--- python/aux_data/apex.py
+++ python/aux_data/apex.py
@@ -4,7 +4,7 @@ from past.builtins import basestring
 import moby2
 
 import numpy as np
-import os, glob
+import os
 
 from . import tools
 
@@ -29,83 +29,7 @@ class WeatherChannel(tools.AuxChannel):
             source_dir = cfg.get('targets_directory')
         if ctime_range is None:
             ctime_range = cfg.get('ctime_range')
-        if ctime_range is None:
-            ctime_range = (0, 1e10)
-
-        self.sources = []
-        self.ctime_range = ctime_range
-        epoch_start, epoch_stop = ctime_range
-
-        self.data = np.zeros((2,0))
-        t_low = epoch_start   # this will prevent us from accidental overlap
-        for t0, t1, filename in get_weather_files(source_dir, channel_name):
-            if t1 < t_low or t0 > epoch_stop:
-                self.sources.append((filename, 0))
-                continue
-            fin = open(filename, 'r')
-            if t0 < t_low:
-                tools.seek_ctime(fin, epoch_start)
-
-            if min(epoch_stop, t1) - max(t_low, t0) > 86400*30:
-                # This call really is only faster for large numbers of
-                # rows...  there is major overhead to the call for some
-                # reason, and since it needs to read to the end of the
-                # file, it does way more IO than necessary in all but
-                # the most extreme cases.
-                data = np.loadtxt(fin, unpack=1)
-                self.data = np.hstack((self.data, np.array(data)))
-            else:
-                data = [[],[]]
-                for line in fin:
-                    entry = line.split()
-                    date = float(entry[0])
-                    if ((date >= epoch_start) and (date < epoch_stop)):
-                        data[0].append(date)
-                        data[1].append(float(entry[1]))
-                    if (date > epoch_stop):
-                        break
-                self.data = np.hstack((self.data, np.array(data)))
-            self.sources.append((filename, len(data[0])))
-            t_low = t1
-
-if 0:
-    def get_nearest(self, ctime):
-        """
-        Finds reading closest to specified ctime.  Returns
-        (channel_value, dtime), where dtime is the (ctime_of_reading -
-        ctime).
-
-        If an array (or list) of ctimes is passed in, arrays of the
-        channel_value and dtime are returned.
-        """
-        if np.asarray(ctime).ndim == 0:
-            ii = np.argmin(abs(self.data[0] - ctime))
-            return self.data[1][ii], self.data[0][ii] - ctime
-        vals, dtimes = np.transpose( list(map(self.get_nearest, ctime)) )
-        return vals, dtimes
-
-    def get_average(self, ctime_start, ctime_end):
-        """
-        Average channel data from times between ctime_start and
-        ctime_end.  Returns (n, mean, std), where n is the number of
-        readings within the given time range.
-        """
-        s = (ctime_start <= self.data[0]) * (self.data[0] < ctime_end)
-        vals = self.data[1][s]
-        if len(vals) == 0:
-            return 0, 0., 0.
-        return len(vals), vals.mean(), vals.std()
-
-    def get_interp(self, spline=True):
-        """
-        Return an interpolator.  Care should be taken to avoid regions
-        where the data are sparse, and especially regions outside of
-        the dataset time range.
-        """
-        import scipy.interpolate as interp
-        if spline:
-            return interp.InterpolatedUnivariateSpline(self.data[0], self.data[1])
-        return interp.interp1d(self.data[0], self.data[1])
+        self._load_ascii_weather(os.path.join(source_dir, 'APEX_%s' % channel_name))
 
 
 class Radiometer(WeatherChannel):
@@ -163,33 +87,3 @@ class WindDirection(WeatherChannel):
     def __init__(self, ctime_range=None, source_dir=None):
         WeatherChannel.__init__(self, 'winddirection', ctime_range=ctime_range,
                                 source_dir=source_dir)
-
-
-
-"""
-Support routines for WeatherChannel.
-"""
-
-def get_weather_files(target_dir, channel):
-    """
-    Find weather data files in target_dir.  Filenames must have format
-
-         APEX_<channel>_<start>_<end>.dat
-
-    Where start and end are ctimes.  Returns a list of tuples
-         (filename, start, end)
-    """
-    if not os.path.exists(target_dir):
-        raise RuntimeError('get_weather_files: directory "%s" does not exist.' % target_dir)
-            
-    files = glob.glob('%s/APEX_%s_*.dat' % (target_dir, channel))
-    data = []
-    for f in files:
-        try:
-            chan, t0, t1 = f.split('/')[-1].rstrip('.dat').split('_')[1:4]
-            data.append((int(t0), int(t1), f))
-        except:
-            raise RuntimeError('get_weather_files: error parsing filename "%s"' % f)
-    return sorted(data)
-
-
