88caa2e9647a55054c417f9147622bc4fc1933a8
Matthew Hasselfield <mhasself@users.noreply.github.com>
2020-05-05 08:20:04 -0400

    aux_data.act: access to ACT radiometer data
    Introduces a new .moby2 key, aux_data.

--- python/aux_data/__init__.py
+++ python/aux_data/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from past.builtins import basestring
+from . import act
 from . import alma
 from . import apex
 from . import hk
